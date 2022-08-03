9736204ab079b5ef0a195a49e62d25676c0e1f17
이기문 <kimoon212@gmail.com>
2022-08-03 12:09:44 +0900

    컴포넌트 수정

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Community from './Community';
+import Side from '../Side';
+
+function Main() {
+  return (
+    <>
+      <Community />
+      <Side />
+    </>
+  );
+}
+
+export default Main;
---
+++ src/components/Side/Side.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Side() {
+  return <div></div>;
+}
+
+export default Side;
