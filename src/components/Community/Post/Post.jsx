b8cecb5752f2fb2d113052d4d8fc9fb5b5a8e00f
Lee-SoYoon <99dlthdbs@gmail.com>
2022-08-04 11:18:18 +0900

    Update ⚡ PostHeader 컴포넌트에 JSON 데이터 추가하기 (오류 발생) #10

--- src/components/Community/Post/Post.jsx
+++ src/components/Community/Post/Post.jsx
@@ -1,19 +1,34 @@
 import React from 'react';
 import styled from 'styled-components';
-import PostHeader from "./PostHeader";
+import PostHeader from './PostHeader';
+import timelineJSON from './data/timeline.json';
+
+  const postArray = fetchJsonToArray(timelineJSON);
 
 function Post() {
-  return (
-    <Wrapper className='post'>
-      <PostHeader />
-      <div className='row2'></div>
-      <div className='row3'></div>
-      <div className='row4'></div>
-    </Wrapper>
-  );
+  postArray.map((el) => {    
+    return (      
+      <Wrapper key={el.id} className='post'>
+        <PostHeader userName={`${el.createdByModifier} ${el.createdBy}`} view={el.view} />
+        <div className='row2'></div>
+        <div className='row3'></div>
+        <div className='row4'></div>
+      </Wrapper>
+    );
+  });
 }
 
 export default Post;
+
+function fetchJsonToArray(rawJson) {
+  const array = [];
+  const jsonData = rawJson.data;
+  for (const key in jsonData) {
+    jsonData[key] = { ...jsonData[key] };
+    array.push(jsonData[key]);
+  }
+  return array;
+}
 
 const Wrapper = styled.div`
   border: 1px solid #e6e6e6;
